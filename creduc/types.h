c5b575161b649fd2086b7ab1a5e3c15f92bc7e80
Jaime de la Cruz Rodriguez <jaime@polar.isp.astro.su.se>
2013-09-02 11:04:50 +0000

    added red_dsgridnest.pro to the repository, and fixed a bug when detecting the number of cells to divide the image. The new function name has been propagated to other routines

--- creduc/types.h
+++ creduc/types.h
@@ -6,12 +6,18 @@ struct complex{
   float64_t re;
   float64_t im;
 };
+typedef struct complex complex_t;
+
 struct fgd{
   float32_t *xl;
   float32_t *yl;
   float32_t *idat;
   float32_t *wav;
   int32_t nmean;
+  complex_t *oftm;
+  complex_t *otft;
+  float64_t *psf;
+  int32_t npsf;
 };
 
 struct pol{
@@ -20,6 +26,5 @@ struct pol{
   float32_t *dat, *qwp, *lp, *cs, *dat2d;
   int32_t ncall;
 };
-typedef struct complex complex_t;
 typedef struct fgd fgd_t;
 typedef struct pol pol_t;
