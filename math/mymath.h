7368cfa13e9fd732ea18329c429633a9a2c53d85
Mats Löfdahl <mats@astro.su.se>
2017-04-26 13:07:27 +0200

    Deleted math subdirectory, cbezier2 and cbezier3 now available as rdx_cbezier2 and rdx_cbezier3.

--- math/mymath.h
+++
@@ -1,4 +0,0 @@
-//
-extern void testmy(int nx, int ny, int type, void *data_in);
-extern double *bezier2(int n, unsigned char *x, unsigned char *y, int np, double *xp, int type);
-extern double *bezier3(int n, unsigned char *x, unsigned char *y, int np, double *xp, int type);
